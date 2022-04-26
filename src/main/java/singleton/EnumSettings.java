9cb9dc8cf8bc5c0062582e976898495444714b0d
ddh963963 <ddh963963@naver.com>
2022-04-26 17:53:50 +0900

    Build : "first commit"

---
+++ src/main/java/singleton/EnumSettings.java
@@ -0,0 +1,9 @@
+package singleton;
+
+public enum EnumSettings {
+    /**
+     *  이렇게 enum 클래스로 객체를 생성하면 안전하게 싱글톤 객체를 만들어 사용할수 있음.
+     *  단점은 상속 받을 수 없음
+     * */
+    INSTANCE;
+}
---
+++ src/main/java/singleton/RuntimeExample.java
@@ -0,0 +1,12 @@
+package singleton;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class RuntimeExample {
+    public static void main(String[] args){
+        Runtime runtime =Runtime.getRuntime();
+        System.out.println(runtime.maxMemory());
+        System.out.println(runtime.freeMemory());
+
+    }
+}
