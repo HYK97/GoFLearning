52cc0a5f39758cb446670304ebc16739bd1754ca
ddh963963 <ddh963963@naver.com>
2022-04-27 18:57:26 +0900

    Feat : Prototype 패턴 추가

---
+++ src/main/java/prototype/apply/Student.java
@@ -0,0 +1,17 @@
+package prototype.apply;
+
+public class Student {
+
+    String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
